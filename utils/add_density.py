e0d8d0c63a4266408994f74363ffe1f869b4f001
luong12320 <nguyenminhluong14092001@gmail.com>
2022-04-30 21:25:22 +0700

    + add file python script store pipeline for adding new column (population density).

---
+++ utils/add_density.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+
+temp_df = pd.read_csv('preprocessed_density.csv')
+temp_df.head()
+
+for i in os.listdir('Worldometer-data-preprocessed'):
+    data_df = pd.read_csv(f'Worldometer-data-preprocessed/{i}')
+
+    pre_df = data_df.join(other=temp_df.set_index('Country'), on='Country')
+    
+    pre_df.to_csv(f'Worldometer-data-preprocessed-2/{i}', index=False)
